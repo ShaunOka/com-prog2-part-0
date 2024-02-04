4dc2e7027ea1651b9d36ce977a60b54145cfcb8f
Shaun Oka <92304647+ShaunOka@users.noreply.github.com>
2024-02-05 00:10:18 +0800

    Add files via upload

--- OkaShaunGeraldStudentRecordsViaParallelArrays.java
+++ OkaShaunGeraldStudentRecordsViaParallelArrays.java
@@ -4,7 +4,23 @@
  * Date: February 6, 2024
  * Instructor: Dale D. Miguel
  */
-
+/*Student Records Keeping Using Parallel Arrays
+*
+* An ID number, a name, a course, a year level and others are
+* attributed to a student. Hence, eight arrays are
+* used: An array that stores ID numbers, an array that
+* stores names, an array that stores courses, an array
+* that stores year levels, etc. The eight arrays are treated
+* as parallel arrays such that the first elements of the
+* eight arrays are actual attributes (ID number, name, course,
+* year level, etc. ) of one student, the second elements are
+* actual attributes of another student, and so on.
+*
+* REQUIRED:
+* Complete and show a sample run (sample output) of the following
+*program.
+*The sample run should be shown below as part of this multiline comment
+**/
 import java.util.Scanner;
 public class OkaShaunGeraldStudentRecordsViaParallelArrays {
     static Scanner scan = new Scanner(System.in);
@@ -23,46 +39,98 @@ public class OkaShaunGeraldStudentRecordsViaParallelArrays {
      sorting and other manipulations
      */
     public void run() throws Exception {
-        int size = readInteger("Please enter size", 1, Integer.MAX_VALUE);
-        String[] id = new String[size];
-        String[] name = new String[size];
-        String[] course = new String[size];
-        int[] yearLevel = new int[size];
-        byte[] prelimGrade = new byte[size];
-        byte[] midtermGrade = new byte[size];
-        byte[] tentativeFinalGrade = new byte[size];
-        byte[] finalGrade = new byte[size];
-        int count;
-        boolean validNumber = false;
-        count = readInteger("Please enter number of student records to be creates", 1, 50);
+        /* TO DO: Instantiate the arrays such that each will have a length=size */
+        int count = readNumber("Please enter number of student records to be created: ", 1, 50);
+        String[] id = new String[count];
+        String[] name = new String[count];
+        String[] course = new String[count];
+        int[] yearLevel = new int[count];
+        byte[] prelimGrade = new byte[count];
+        byte[] midtermGrade = new byte[count];
+        byte[] tentativeFinalGrade = new byte[count];
+        byte[] finalGrade = new byte[count];
+        /* Read the number of students to be recorded from the keyboard */
+        /* When testing your program, enter a few number of students (e.g. 5) so
+        that your test will not need a lot of inputs */
+        /* TO DO: Invoke the method that will fill the arrays with elements */
         populateArrays(id, name, course, yearLevel, prelimGrade, midtermGrade, tentativeFinalGrade,
                 finalGrade);
+        /* Show the students following the order by which they were entered */
+        System.out.println("Original Order Data");
+        displayArrays(id, name, course, yearLevel, prelimGrade, midtermGrade, tentativeFinalGrade, finalGrade);
+        //TO DO: Invoke the method for displaying the array elements
+
     }
-    public int readInteger(String prompt, int min, int max) {
+    public int readNumber(String prompt, int min, int max) {
         int value = -1;
         boolean validValue = false;
         while (!validValue) {
             try {
-                System.out.printf("%s: ", prompt);
+                System.out.printf("%s", prompt);
                 value = Integer.parseInt(scan.nextLine());
                 if (value<min || value>max) {
-                    System.out.printf("Please enter a number greater than %d and lower than %d%n", min, max);
+                    System.out.printf("Please enter a number minimum of %d and maximum of %d%n", min, max);
                 } else {
                     validValue = true;
                 }
             } catch (NumberFormatException numberException) {
                 validValue = false;
-                System.out.println("ERROR: Invalid integer.");
+                System.out.println("ERROR: Invalid number.");
                 System.out.println("Please try again.");
             }
         }
         return value;
     }
     public void populateArrays(String[] id, String[] name, String[] course, int[] yearLevel,
-                               byte[] prelimGrade, byte[] midtermGrade, byte[] tentativeFinalGrade,
-                               byte[] finalGrade) {
-        String[][] stringStudentRecords = {id, name, course};
-        byte[][] byteStudentRecords = {prelimGrade, midtermGrade, tentativeFinalGrade, finalGrade};
-
+        byte[] prelimGrade, byte[] midtermGrade, byte[] tentativeFinalGrade, byte[] finalGrade) {
+        //TO DO. Insert appropriate lines of codes
+        /* When accepting a value of prelim grade, midterm grade and tentative final
+        grade, validate the input in order to ensure that the value entered is an integer
+        in the range 65 to 99 */
+        /* The final grade should be computed as the average of the prelim grade,
+        Midterm grade and tentative final grade */
+        for (int index = 0; index < id.length; index++) {
+            System.out.printf("Student record (%d):%n", index+1);
+            System.out.print("Please enter id number: ");
+            id[index] = scan.nextLine();
+            System.out.print("Please enter name: ");
+            name[index] = scan.nextLine();
+            System.out.print("Please enter course: ");
+            course[index] = scan.nextLine();
+            yearLevel[index] = readNumber("Please enter year level: ", 1, 7);
+            System.out.print("Please enter preliminary grade: ");
+            prelimGrade[index] = (byte)(readNumber("", 65, 99));
+            System.out.print("Please enter midterm grade: ");
+            midtermGrade[index] = (byte)(readNumber("", 65, 99));
+            System.out.print("Please enter tentative final grade: ");
+            tentativeFinalGrade[index] = (byte)(readNumber("", 65, 99));
+            finalGrade[index] = (byte)((prelimGrade[index]+midtermGrade[index]+tentativeFinalGrade[index])/finalGrade.length);
+        }
+    }
+    public void displayArrays(String[] id, String[] name, String[] course, int[] yearLevel,
+    byte[] prelimGrade, byte[] midtermGrade, byte[] tentativeFinalGrade, byte[] finalGrade) {
+        for (int index = 0; index <= 50; index++) {
+            System.out.print("<");
+        }
+        System.out.print("-");
+        for (int index = 0; index <= 50; index++) {
+            System.out.print(">");
+        }
+        System.out.println();
+        System.out.printf("%46s%n", "Student Record");
+        System.out.printf("%20s%11s%11s%11s%11s%11s%17s%11s%n", "Name", "ID", "Course", "Year", "Prelims", "Midterm", "Tentative Final", "Finals");
+        for (int index = 0; index < id.length; index++) {
+            System.out.printf("%20s%11s%11s%11d%11d%11d%17d%11d%n", name[index], id[index], course[index],
+             yearLevel[index], prelimGrade[index], midtermGrade[index], tentativeFinalGrade[index],
+             finalGrade[index]);
+        }
+        System.out.println();
+        for (int index = 0; index <= 50; index++) {
+            System.out.print("<");
+        }
+        System.out.print("-");
+        for (int index = 0; index <= 50; index++) {
+            System.out.print(">");
+        }
     }
 }
