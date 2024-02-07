4ae608b6b386a653c1cd3de0c0b2cec4ae669305
Shaun Oka <92304647+ShaunOka@users.noreply.github.com>
2024-02-07 15:00:00 +0800

    Update OkaShaunGeraldStudentRecordsViaParallelArrays.java

--- OkaShaunGeraldStudentRecordsViaParallelArrays.java
+++ OkaShaunGeraldStudentRecordsViaParallelArrays.java
@@ -5,23 +5,23 @@
  * Instructor: Dale D. Miguel
  */
 /*
-* Student Records Keeping Using Parallel Arrays
-*
-* An ID number, a name, a course, a year level and others are
-* attributed to a student. Hence, eight arrays are
-* used: An array that stores ID numbers, an array that
-* stores names, an array that stores courses, an array
-* that stores year levels, etc. The eight arrays are treated
-* as parallel arrays such that the first elements of the
-* eight arrays are actual attributes (ID number, name, course,
-* year level, etc. ) of one student, the second elements are
-* actual attributes of another student, and so on.
-*
-* REQUIRED:
-* Complete and show a sample run (sample output) of the following
-*program.
-*The sample run should be shown below as part of this multiline comment
-**/
+ * Student Records Keeping Using Parallel Arrays
+ *
+ * An ID number, a name, a course, a year level and others are
+ * attributed to a student. Hence, eight arrays are
+ * used: An array that stores ID numbers, an array that
+ * stores names, an array that stores courses, an array
+ * that stores year levels, etc. The eight arrays are treated
+ * as parallel arrays such that the first elements of the
+ * eight arrays are actual attributes (ID number, name, course,
+ * year level, etc. ) of one student, the second elements are
+ * actual attributes of another student, and so on.
+ *
+ * REQUIRED:
+ * Complete and show a sample run (sample output) of the following
+ *program.
+ *The sample run should be shown below as part of this multiline comment
+ **/
 import java.util.Scanner;
 public class OkaShaunGeraldStudentRecordsViaParallelArrays {
     static Scanner scan = new Scanner(System.in);
@@ -97,7 +97,7 @@ public class OkaShaunGeraldStudentRecordsViaParallelArrays {
      * final grade
      **/
     public void populateArrays(String[] id, String[] name, String[] course, int[] yearLevel,
-        byte[] prelimGrade, byte[] midtermGrade, byte[] tentativeFinalGrade, byte[] finalGrade) {
+                               byte[] prelimGrade, byte[] midtermGrade, byte[] tentativeFinalGrade, byte[] finalGrade) {
         //TO DO. Insert appropriate lines of codes
         /* When accepting a value of prelim grade, midterm grade and tentative final
         grade, validate the input in order to ensure that the value entered is an integer
@@ -135,8 +135,8 @@ public class OkaShaunGeraldStudentRecordsViaParallelArrays {
         System.out.printf("%n%20s%11s%17s%7s%10s%11s%18s%9s%n", "Name", "ID", "Course", "Year", "Prelims", "Midterms", "Tentative Final", "Finals");
         for (int index = 0; index < id.length; index++) {
             System.out.printf("%20s%11s%17s%7d%10d%11d%18d%9d%n", name[index], id[index], course[index],
-             yearLevel[index], prelimGrade[index], midtermGrade[index], tentativeFinalGrade[index],
-             finalGrade[index]);
+                    yearLevel[index], prelimGrade[index], midtermGrade[index], tentativeFinalGrade[index],
+                    finalGrade[index]);
         }
         System.out.println();
         for (int index = 0; index <= 50; index++) {
@@ -157,84 +157,99 @@ public class OkaShaunGeraldStudentRecordsViaParallelArrays {
      */
     public void sortArrays(String[] id, String[] name, String[] course, int[] yearLevel,
                            byte[] prelimGrade, byte[] midtermGrade, byte[] tentativeFinalGrade, byte[] finalGrade) {
-        int sortIndex = 0;
 
         for (int index1 = 0; index1 < name.length; index1++) {
+            int sortIndex = index1;
             for (int index2 = index1 + 1; index2 < name.length; index2++) {
-                if (name[index1].compareTo(name[index2]) > 0) {
+                if (name[sortIndex].compareTo(name[index2]) > 0) {
                     sortIndex = index2;
                 }
             }
-            String tempName = name[sortIndex];
-            name[sortIndex] = name[index1];
-            name[index1] = tempName;
+            if (!name[sortIndex].equals(name[index1])) {
+                String tempName = name[sortIndex];
+                name[sortIndex] = name[index1];
+                name[index1] = tempName;
 
-            String tempID = id[sortIndex];
-            id[sortIndex] = id[index1];
-            id[index1] = tempID;
+                String tempID = id[sortIndex];
+                id[sortIndex] = id[index1];
+                id[index1] = tempID;
 
-            String tempCourse = course[sortIndex];
-            course[sortIndex] = course[index1];
-            course[index1] = tempCourse;
+                String tempCourse = course[sortIndex];
+                course[sortIndex] = course[index1];
+                course[index1] = tempCourse;
 
-            int tempYearLevel = yearLevel[sortIndex];
-            yearLevel[sortIndex] = yearLevel[index1];
-            yearLevel[index1] = tempYearLevel;
+                int tempYearLevel = yearLevel[sortIndex];
+                yearLevel[sortIndex] = yearLevel[index1];
+                yearLevel[index1] = tempYearLevel;
 
-            byte tempPrelim = prelimGrade[sortIndex];
-            prelimGrade[sortIndex] = prelimGrade[index1];
-            prelimGrade[index1] = tempPrelim;
+                byte tempPrelim = prelimGrade[sortIndex];
+                prelimGrade[sortIndex] = prelimGrade[index1];
+                prelimGrade[index1] = tempPrelim;
 
-            byte tempMidterm = midtermGrade[sortIndex];
-            midtermGrade[sortIndex] = midtermGrade[index1];
-            midtermGrade[index1] = tempMidterm;
+                byte tempMidterm = midtermGrade[sortIndex];
+                midtermGrade[sortIndex] = midtermGrade[index1];
+                midtermGrade[index1] = tempMidterm;
 
-            byte tempTFinal = tentativeFinalGrade[sortIndex];
-            tentativeFinalGrade[sortIndex] = tentativeFinalGrade[index1];
-            tentativeFinalGrade[index1] = tempTFinal;
+                byte tempTFinal = tentativeFinalGrade[sortIndex];
+                tentativeFinalGrade[sortIndex] = tentativeFinalGrade[index1];
+                tentativeFinalGrade[index1] = tempTFinal;
 
-            byte tempFinal = finalGrade[sortIndex];
-            finalGrade[sortIndex] = finalGrade[index1];
-            finalGrade[index1] = tempFinal;
+                byte tempFinal = finalGrade[sortIndex];
+                finalGrade[sortIndex] = finalGrade[index1];
+                finalGrade[index1] = tempFinal;
+            } // end of if
         } // end of nested loops
     } // end of sortArrays
 }
 /*
+SAMPLE OUTPUT
+Please enter number of student records to be created: a
+ERROR: Invalid number.
+Please try again.
+Please enter number of student records to be created: 0
+Please enter a number minimum of 1 and maximum of 50
+Please enter number of student records to be created: 500
+Please enter a number minimum of 1 and maximum of 50
 Please enter number of student records to be created: 3
 Student record (1):
 Please enter id number: 123
-Please enter name: Oka, Shaun
+Please enter name: Shaun
 Please enter course: BSCS
 Please enter year level: 1
+Please enter preliminary grade: a
+ERROR: Invalid number.
+Please try again.
+Please enter preliminary grade: 1000
+Please enter a number minimum of 65 and maximum of 99
+Please enter preliminary grade: 4
+Please enter a number minimum of 65 and maximum of 99
+Please enter preliminary grade: 99
+Please enter midterm grade: 99
+Please enter tentative final grade: 99
+Student record (2):
+Please enter id number: 124
+Please enter name: Gerald
+Please enter course: BSIT
+Please enter year level: 2
 Please enter preliminary grade: 99
 Please enter midterm grade: 98
 Please enter tentative final grade: 97
-Student record (2):
-Please enter id number: 124
-Please enter name: B, Shaun
+Student record (3):
+Please enter id number: 125
+Please enter name: Oka
 Please enter course: BSMMA
-Please enter year level: 96
-Please enter a number minimum of 1 and maximum of 7
-Please enter year level: 2
+Please enter year level: 3
 Please enter preliminary grade: 96
 Please enter midterm grade: 95
 Please enter tentative final grade: 94
-Student record (3):
-Please enter id number: 125
-Please enter name: Y, Oka
-Please enter course: BSIT
-Please enter year level: 3
-Please enter preliminary grade: 93
-Please enter midterm grade: 92
-Please enter tentative final grade: 91
 
 Original Order Data
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
                 Name         ID           Course   Year   Prelims   Midterms   Tentative Final   Finals
-          Oka, Shaun        123             BSCS      1        99         98                97       98
-            B, Shaun        124            BSMMA      2        96         95                94       95
-              Y, Oka        125             BSIT      3        93         92                91       92
+               Shaun        123             BSCS      1        99         99                99       99
+              Gerald        124             BSIT      2        99         98                97       98
+                 Oka        125            BSMMA      3        96         95                94       95
 
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -242,11 +257,12 @@ Sorted Data
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
                 Name         ID           Course   Year   Prelims   Midterms   Tentative Final   Finals
-            B, Shaun        124            BSMMA      2        96         95                94       95
-          Oka, Shaun        123             BSCS      1        99         98                97       98
-              Y, Oka        125             BSIT      3        93         92                91       92
+              Gerald        124             BSIT      2        99         98                97       98
+                 Oka        125            BSMMA      3        96         95                94       95
+               Shaun        123             BSCS      1        99         99                99       99
 
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 Process finished with exit code 0
+
  */
