c584db635efc40b5ace97ed90f1ca607be877a8d
Shaun Oka <92304647+ShaunOka@users.noreply.github.com>
2024-03-26 18:27:48 +0800

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -15,7 +15,7 @@ public class Triangle extends Shape {
     public double area() {
         double area;
         double semiPerimeter = perimeter()/2;
-        area = Math.sqrt((semiPerimeter*(semiPerimeter-side1)*(semiPerimeter-side2)*(semiPerimeter-side3)));
+        area = Math.sqrt(semiPerimeter*(semiPerimeter-side1)*(semiPerimeter-side2)*(semiPerimeter-side3));
         return area;
     }
 
